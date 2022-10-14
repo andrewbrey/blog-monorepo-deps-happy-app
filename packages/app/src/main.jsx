e5a813108afe894497d744b988e6b9320e23625b
Andrew Brey <34140052+andrewbrey@users.noreply.github.com>
2022-10-13 18:11:42 -0700

    chore: remove comment

--- packages/app/src/main.jsx
+++ packages/app/src/main.jsx
@@ -1,6 +1,3 @@
-// In this application, I _had_ to do any react 17/18 migrations
-// because now the whole repo on has only one react version!
-
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
